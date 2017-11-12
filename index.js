8a444d4aff3de039e501bbcfa1b1443238136270
carlitos321 <ca.cabrera321@gmail.com>
2017-11-12 20:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
 
-let shortGreeting = `Welcome, ${currentUser}!`;
+let shortGreeting = `Welcome, ${currentUser.substring(0,1)}!`;
