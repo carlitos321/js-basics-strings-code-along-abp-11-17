ba6f1fc9045e22531e702276f6f39f4832e4943f
carlitos321 <ca.cabrera321@gmail.com>
2017-11-12 20:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = 'John'
 
-let welcomeMessage = 'Welcome to Flatbook, ' + currentUser
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
