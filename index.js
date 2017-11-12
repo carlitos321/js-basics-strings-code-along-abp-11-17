90e243ab354d2b43809c5e52d75dda983d3a8023
carlitos321 <ca.cabrera321@gmail.com>
2017-11-12 20:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 const currentUser = 'John';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser}!';
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
 
-let shortGreeting = 'Welcome, ${currentUser}!';
+let shortGreeting = `Welcome, ${currentUser}!`;
